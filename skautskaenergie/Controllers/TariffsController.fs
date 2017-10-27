bc1ae7d6b261d959e2f40c20a074eb2023ada484
Pavel Kučera <pavel.kucera@edhouse.cz>
2017-10-27 04:25:28 +0200

    +skautskaenergie - initial API draft

---
+++ skautskaenergie/Controllers/TariffsController.fs
@@ -0,0 +1,19 @@
+﻿namespace skautskaenergie.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+
+[<Route("api/[controller]")>]
+type TariffsController () =
+    inherit Controller()
+
+    [<HttpGet>]
+    member this.Get() =
+        [|"D01d"; "D02d" ; "..."|]
+
+    [<HttpGet("{id}")>]
+    member this.Get(id:string) =
+        id
