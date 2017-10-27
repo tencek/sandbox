583b86f5d768066ab22d0b04a63a4c22eca5ab94
Pavel Kučera <pavel.kucera@edhouse.cz>
2017-10-27 05:24:33 +0200

    skautskaenergie + Tariff.fs

---
+++ skautskaenergie/Tariff.fs
@@ -0,0 +1,26 @@
+module skautskaenergie.Tariff
+
+[<Measure>] type CZK
+
+type Tariff = 
+    | SingleRateTariff of SingleRateTariff
+    | DoubleRateTariff of DoubleRateTariff
+
+and SingleRateTariff = {
+    Name: TariffName
+    Price: Price
+}
+
+and DoubleRateTariff = {
+    Name: TariffName
+    PriceHigh: Price
+    PriceLow: Price
+}
+
+and TariffName = string
+and Price = decimal<CZK>
+
+let _gas = SingleRateTariff { Name = "Plyn" ; Price = 497m<CZK> }
+let _D25d = DoubleRateTariff { Name = "D25d" ; PriceHigh = 1090m<CZK> ; PriceLow = 765m<CZK> }
+
+let tariffs = [ _gas ; _D25d ]
