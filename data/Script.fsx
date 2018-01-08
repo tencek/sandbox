b4c86b85f96f693ced2e8ca10bbe786fce210b89
Pavel Kučera <pavel.kucera@edhouse.cz>
2018-01-08 08:32:37 +0100

    add temperatures.fsx

--- data/Script.fsx
+++ data/Script.fsx
@@ -6,47 +6,11 @@
 open FSharp.Data
 open System.Text.RegularExpressions
 open System
-open System.Web.UI.WebControls
+open System.Globalization
 
-[<Measure>] type degC
-
-let (|TemperatureInCelsius|_|) str =
-   let m = Regex.Match(str,"(.*)°C") 
-   if (m.Success) then 
-        (m.Groups.[1].Value |> float) * 1.0<degC> |> Some
-   else 
-        None  
-
-let makeUrl stanice (date:DateTime) = 
-    sprintf "http://www.in-pocasi.cz/meteostanice/stanice-historie.php?stanice=%s&historie=%s" stanice (date.ToString("MM-dd-yyyy"))
-
-type InPocasi = 
-  HtmlProvider<"http://www.in-pocasi.cz/meteostanice/stanice-historie.php?stanice=zlin&historie=11-29-2017">
-
-InPocasi().Tables.Table1.Rows
-|> Seq.map (fun row -> row.Teplota)
-|> Seq.map(function
-    | TemperatureInCelsius temp -> Some temp
-    | _ -> None)
-|> Seq.choose(id)
-|> Seq.average
-|> printfn "Průměrná teplota: %A"
-
-let parseDate dateStr =
-    DateTime.Parse(dateStr, )
-
-let fromDate  = parseDate "7.1.2017 10:40:00"
-let toDate = parseDate "9.1.2017 22:35:00"
-
-let dateList (fromDate:DateTime) (toDate:DateTime) = 
-    toDate.Date - fromDate.Date
-    //seq { for i in fromDate .. toDate -> i }
-    //seq { yield fromDate.Date ; yield toDate.Date }
-
-printfn "%A" (toDate.Date - fromDate.Date).Days
-printfn "%A" toDate
-
-dateList fromDate toDate |> printfn "%A"
+let trace label x =
+    printfn "%s: %A" label x
+    x
 
 type Zastavky =  HtmlProvider<"http://www.dszo.cz/komunikace/?page=zastavky">
 
