1ceac42571b920972ae49bed9b5ba3ba86fd1c95
Pavel Kučera <pavel.kucera@edhouse.cz>
2018-01-08 19:25:01 +0100

    + drevnice

---
+++ data/drevnice.fsx
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+//#I @"bin\Release"
+#r @"C:\git\sandbox\packages\FSharp.Data.2.4.2\lib\net45\FSharp.Data.dll"
+
+open FSharp.Data
+
+type Drevnice = 
+  HtmlProvider<"http://hydro.chmi.cz/hpps/hpps_prfdata.php?seq=307366", Encoding="utf-8">
+
+
+Drevnice().Tables.Table8.Rows.[0].``Stav [cm]`` |> printfn "%A"
+
