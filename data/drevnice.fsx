c90a1cb01d8ed18f49cc3ff7b9cc0e8aac7b8bd5
Pavel Kučera <pavel.kucera@edhouse.cz>
2018-03-03 09:22:44 +0100

    drevnice + fsharp.charting

--- data/drevnice.fsx
+++ data/drevnice.fsx
@@ -2,17 +2,30 @@
 // for more guidance on F# programming.
 //#I @"bin\Release"
 #r @"C:\git\sandbox\packages\FSharp.Data.2.4.2\lib\net45\FSharp.Data.dll"
+#r @"C:\Users\PK250193\.nuget\packages\fsharp.charting\0.91.1\lib\net45\FSharp.Charting.dll"
 
 open FSharp.Data
+open FSharp.Charting
 
 type Drevnice = 
   HtmlProvider<"http://hydro.chmi.cz/hpps/hpps_prfdata.php?seq=307366", Encoding="utf-8">
 
 Drevnice().Tables.Table8.Rows.[0].``Stav [cm]`` |> printfn "Actual: %A"
 
-Drevnice().Tables.Table8.Rows
-|> Seq.ofArray
-|> Seq.take 10
-|> Seq.rev
-|> Seq.iter (fun row -> printfn "%A: %A cm." row.``Datum a Äas`` row.``Stav [cm]``)
+let rows = 
+    Drevnice().Tables.Table8.Rows
+    |> Seq.cache
 
+let tempGraph = 
+    rows
+    |> Seq.map (fun row -> (row.``Datum a Äas``, row.``Teplota [Â°C]``))
+    |> Chart.Line
+
+let levelGraph = 
+    rows
+//    |> Seq.filter (fun row -> row.``Stav [cm]`` > 28)
+    |> Seq.map (fun row -> (row.``Datum a Äas``, row.``Stav [cm]``))
+    |> Chart.Line
+
+Chart.Combine [tempGraph ; levelGraph ]
+|> Chart.Show
