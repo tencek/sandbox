a4a27db82d2076741d9b746b555eea157f8a441e
Pavel Kučera <pavel.kucera@edhouse.cz>
2018-03-06 23:36:27 +0100

    swagger working!

--- klubovna.api/Startup.fs
+++ klubovna.api/Startup.fs
@@ -8,7 +8,8 @@ open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
-
+open Swashbuckle.AspNetCore
+open Swashbuckle.AspNetCore.Swagger
 
 type Startup private () =
     new (configuration: IConfiguration) as this =
@@ -19,9 +20,14 @@ type Startup private () =
     member this.ConfigureServices(services: IServiceCollection) =
         // Add framework services.
         services.AddMvc() |> ignore
+        services.AddSwaggerGen(fun c -> 
+            c.SwaggerDoc("v1", new Info(Title="Klubovna API", Version="v1"))) |> ignore
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     member this.Configure(app: IApplicationBuilder, env: IHostingEnvironment) =
         app.UseMvc() |> ignore
+        app.UseSwagger() |> ignore
+        app.UseSwaggerUI(fun c -> 
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Klubovna API V1")) |> ignore
 
     member val Configuration : IConfiguration = null with get, set
