c2aa3c72866f1725248ca35de0efb0349da3722e
Pavel Kučera <pavel.kucera@edhouse.cz>
2018-03-25 21:07:50 +0200

    +calendar

--- data/Script.fsx
+++ data/Script.fsx
@@ -32,3 +32,12 @@ Linka11().Tables.Table12.Rows |> Seq.iter ( fun row -> row.ToValueTuple() |> pri
 type Linka11Gemini = HtmlProvider<"http://www.dszo.cz/jr/workhtml.php?htmlfile=MjAxNy0xMC0xMS8xMWFfMDkuaHRtbA==&version=0">
 Linka11Gemini().Tables.Table5.Rows |> Seq.iter ( fun row -> row |> printfn "| ROW: %A" )
 Linka11Gemini().Tables.Table6.Rows |> Seq.iter ( fun row -> row |> printfn "| ROW: %A" )
+
+
+////////////////////////////
+
+type Calendar = JsonProvider<"https://ical-to-json.herokuapp.com/convert.json?url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2Fzlin6.cz_822fssjj8hlr2khh6tt6v5d1qc%2540group.calendar.google.com%2Fpublic%2Fbasic.ics">
+let x = 
+    Calendar.Load("https://ical-to-json.herokuapp.com/convert.json?url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2Fzlin6.cz_822fssjj8hlr2khh6tt6v5d1qc%2540group.calendar.google.com%2Fpublic%2Fbasic.ics").Vcalendar.[0].Vevent
+    |> Seq.choose (fun event -> event.Rrule)
+    |> Seq.iter (fun rule -> printfn "%A" rule)
