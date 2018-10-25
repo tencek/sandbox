5f011abcdbda741845d98f5f47dba64a1ead394f
Pavel Kučera <pavel.kucera@edhouse.cz>
2018-10-25 21:18:31 +0200

    Seq

---
+++ data/Seq.fsx
@@ -0,0 +1,12 @@
+﻿seq { 1 .. 4 }
+|> Seq.allPairs <| seq { 1 .. 3 }
+|> Seq.iter (printf "%A")
+
+seq { 1 .. 9 }
+|> Seq.append <| seq { 1 .. 3 }
+|> Seq.iter (printf "%A")
+
+seq { 1.0 .. 55.0 }
+|> Seq.average
+|> printfn "%A"
+
