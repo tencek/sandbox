6e40c2bcb95b9f3c9feef7b8f52ce1116bf0ccec
Pavel Kučera <pavel.kucera@edhouse.cz>
2018-11-20 08:07:06 +0100

    Some imrovements

--- data/Addresses.fsx
+++ data/Addresses.fsx
@@ -11,9 +11,11 @@ let BaseUrl = "http://apl.czso.cz/irso4"
 
 type Address = {Street:string ; Number:int}
 
-let MakeAddress street number =
-  { Street=street
-    Number=number }
+type HtmlData = HtmlData of string
+
+type Range = { Min:int ; Max:int }
+
+let MakeAddress street number = { Street=street ; Number=number }
 
 let UrlEncode text = 
     let enc = Encoding.GetEncoding(1250)
@@ -27,7 +29,7 @@ let RequestString url =
     Thread.Sleep 500
     Http.RequestString(url)
 
-let GetDetailsUrl searchPageContent = 
+let GetDetailsUrl (HtmlData searchPageContent) = 
     let m = Regex.Match(searchPageContent, """<a title= "Detailní údaje o budově" href="(.*)">Detail...</a>""")
     if m.Success 
     then sprintf "%s/%s" <| BaseUrl <| m.Groups.[1].Value |> Some 
@@ -35,7 +37,7 @@ let GetDetailsUrl searchPageContent =
         printfn "DATA ERROR!"
         None
 
-let GetNumberOfPeople detailsPageContent =
+let GetNumberOfPeople (HtmlData detailsPageContent) =
     let m = Regex.Match(detailsPageContent, """<tr>
         <td>Počet evidovaných obyvatel v budově:</td>
         <td>.*</td>
@@ -43,7 +45,7 @@ let GetNumberOfPeople detailsPageContent =
         <td>(\d+) ?- ?(\d+)</td>
       </tr>""")
     if m.Success 
-    then (int m.Groups.[1].Value, int m.Groups.[2].Value) |> Some 
+    then {Min=int m.Groups.[1].Value; Max=int m.Groups.[2].Value} |> Some 
     else
         let m = Regex.Match(detailsPageContent, """<tr>
             <td>Počet evidovaných obyvatel v budově:</td>
@@ -52,7 +54,7 @@ let GetNumberOfPeople detailsPageContent =
             <td>(\d+)</td>
           </tr>""")
         if m.Success 
-        then (int m.Groups.[1].Value, int m.Groups.[1].Value) |> Some 
+        then {Min=int m.Groups.[1].Value; Max=int m.Groups.[1].Value} |> Some 
         else
             let m = Regex.Match(detailsPageContent, """<tr>
         <td>Počet obyvatel v budově dle SLDB - trvalý pobyt:</td>
@@ -70,62 +72,68 @@ let GetNumberOfPeople detailsPageContent =
             then 
                 let a = int m.Groups.[1].Value
                 let b = int m.Groups.[2].Value
-                (min a b, max a b) |> Some
+                {Min=min a b; Max=max a b} |> Some
             else
                 printfn "DATA ERROR 2!"
                 None
 
-let GetNumberOfFlats detailsPageContent =
+let GetNumberOfFlats (HtmlData detailsPageContent) =
     let m = Regex.Match(detailsPageContent, """<tr>
         <td>Počet bytů v budově \(vchodu\):</td>
         <td>(\d+)</td>""")
     if m.Success 
-    then (int m.Groups.[1].Value, int m.Groups.[1].Value) |> Some 
+    then {Min=int m.Groups.[1].Value; Max=int m.Groups.[1].Value} |> Some 
     else
         printfn "DATA ERROR 3!"
         None
 
 [<Literal>]
 let dataFile = __SOURCE_DIRECTORY__ + @"\Assets\adresy-okoli-400m.csv"
-type Data = CsvProvider<dataFile, Separators=";">
+type Adresses = CsvProvider<dataFile, Separators=";">
 
 
-let data = 
-    (new Data()).Rows
-    |> Seq.map (fun row -> (row.``Název ulice``, row.``Číslo domovní``))
+//let addresses = 
+//    (new Adresses()).Rows
+//    |> Seq.map (fun row -> (row.``Název ulice``, row.``Číslo domovní``))
 
-//let data = 
-//    [
-//        ("Křiby",4711)
-//        //("Křiby",4712) 
-//    ]
+let addresses = 
+    [
+        ("Křiby",4711)
+        //("Křiby",4712) 
+    ]
 
 let htmlData = 
-    data
+    addresses
     |> Seq.map (fun pair -> MakeAddress <| fst pair <| snd pair )
-    |> Seq.map MakeUrl
-    |> Seq.map RequestString
-    |> Seq.map GetDetailsUrl
+    |> Seq.map (fun address -> 
+        address
+        |> MakeUrl
+        |> RequestString
+        |> HtmlData
+        |> GetDetailsUrl
+        |> Option.map (fun url -> url.Replace("&amp;","&") |> RequestString |> HtmlData )
+        |> Option.map (fun html -> (address, html)))
     |> Seq.choose id
-    |> Seq.map (fun url -> url.Replace("&amp;","&"))
-    |> Seq.map (fun url -> (url, RequestString url))
     |> Seq.cache
+
+let numbers = 
+    htmlData
+    |> Seq.map (fun (address, html) ->
+        (address, (GetNumberOfPeople html), (GetNumberOfFlats html))
+    )
 
 let people = 
     htmlData
     |> Seq.map (snd >> GetNumberOfPeople)
     |> Seq.choose id
 
-let minpeople = people |> Seq.sumBy (fun minmax -> fst minmax)
-let maxpeople = people |> Seq.sumBy (fun minmax -> snd minmax)
+let minpeople = people |> Seq.sumBy (fun range -> range.Min)
+let maxpeople = people |> Seq.sumBy (fun range -> range.Max)
 
 let flats = 
     htmlData
     |> Seq.map (snd >> GetNumberOfFlats)
     |> Seq.choose id
 
-let minflats = flats |> Seq.sumBy (fun minmax -> fst minmax)
-let maxflats = flats |> Seq.sumBy (fun minmax -> snd minmax)
-
-
-Http.RequestString("http://apl.czso.cz/irso4/buddet.jsp?budid=3321999&idob=1018991417&b=11&textobce=Zl%EDn&textulice=K%F8iby&cisdom=4711")
+let minflats = flats |> Seq.sumBy (fun range -> range.Min)
+let maxflats = flats |> Seq.sumBy (fun range -> range.Max)
