21a4c8127b3600c694a57cbdd3c679f0385bac1d
Pavel Kučera <pavel.kucera@edhouse.cz>
2019-01-01 23:14:33 +0100

    + get-temp

---
+++ get-temp/gettemp.fs
@@ -0,0 +1,51 @@
+﻿module gettemp
+
+open CommandLine
+open System.Net.Http
+open System.Text.RegularExpressions
+
+
+type Options = {
+  [<Option('l', "list-all", Required = false)>] listAll : bool;
+}
+
+let getAllAsync () = async {
+        use client = new HttpClient()
+        let! html = 
+            client.GetStringAsync("https://www.in-pocasi.cz/meteostanice/")
+            |> Async.AwaitTask
+
+        let regex = new Regex(Regex.Escape("""href="stanice.php?stanice=""")+"""([^"]+)""")
+        return
+            regex.Matches(html)
+            |> Seq.cast<Match>
+            |> Seq.map (fun mat -> mat.Groups.[1].Value)
+            |> Seq.sort
+            |> Seq.distinct
+}
+
+let listAll () =
+    getAllAsync ()
+    |> Async.RunSynchronously
+    |> Seq.iter (printfn "%s")
+    0
+
+let run (options:Options) =
+    if options.listAll then
+        listAll ()
+    else
+        0
+
+let fail errors = 
+    printfn "Failed: %A" errors
+    -1
+
+[<EntryPoint>]
+let main argv =
+    let cmdParseResult = CommandLine.Parser.Default.ParseArguments<Options>(argv)
+    let retVal = 
+        match cmdParseResult with
+        | :? Parsed<Options> as parsed -> run parsed.Value
+        | :? NotParsed<Options> as notParsed -> fail notParsed.Errors
+        | _ -> -1
+    retVal
---
+++ get-temp/gettemp.fsx
@@ -0,0 +1,38 @@
+﻿#r "netstandard"
+#r "System.Net.Http"
+#r @"C:\Users\Manžel\.nuget\packages\fsharp.data\3.0.0\lib\netstandard2.0\FSharp.Data.dll"
+
+open System.Net.Http
+open System.Text.RegularExpressions
+open FSharp.Data
+open System
+
+let getAllAsync () = async {
+        use client = new HttpClient()
+        let! html = 
+            client.GetStringAsync("https://www.in-pocasi.cz/meteostanice/")
+            |> Async.AwaitTask
+
+        let regex = new Regex(Regex.Escape("""href="stanice.php?stanice=""")+"""([^"]+)""")
+        return
+            regex.Matches(html)
+            |> Seq.cast<Match>
+            |> Seq.map (fun mat -> mat.Groups.[1].Value)
+            |> Seq.sort
+            |> Seq.distinct
+}
+
+let listAll () =
+    getAllAsync ()
+    |> Async.RunSynchronously
+    |> Seq.iter (printfn "%s")
+    0
+
+type InPocasiNow = FSharp.Data HtmlProvider<"https://www.in-pocasi.cz/meteostanice/stanice.php?stanice=zlin_centrum", Encoding="utf-8">
+
+//InPocasiNow().Tables.Table1.Rows.[0]
+//|> (fun row -> (row.``Äas``, row.Teplota))
+//|> (fun (time, temp) -> printfn "Teplota v %s byla %s." (time.ToString()) temp)
+
+
+listAll ()
