53e498daace6579c9283c7f0419c5420f9b0c225
Pavel Kučera <pavel.kucera@edhouse.cz>
2020-03-08 04:24:54 +0100

    registrace - 2020, first tests

--- data/registrace.fsx
+++ data/registrace.fsx
@@ -2,25 +2,63 @@
 // for more guidance on F# programming.
 //#I @"bin\Release"
 #r @"..\packages\FSharp.Data.3.0.0\lib\net45\FSharp.Data.dll"
-#r @"..\packages\FSharp.Charting.2.1.0\lib\net45\FSharp.Charting.dll"
 
 open FSharp.Data
-open FSharp.Charting
+open System
 
 [<Literal>]
-let registraceDataFile = __SOURCE_DIRECTORY__ + @"\Assets\registrace-all-anonymized.csv"
-type Registrace = CsvProvider<registraceDataFile, Encoding="utf-8">
+let RegistraceDataFile = __SOURCE_DIRECTORY__ + @"\Assets\registrace-all-anonymized.csv"
+type Registrace = CsvProvider<RegistraceDataFile, Encoding="utf-8", Schema="Id=string">
+
+type MemberStats = {
+   id:string
+   yearIn:int
+   ageIn:int
+   yearOut: int option 
+   ageOut:int option
+   regs:seq<Registrace.Row>
+}
 
 let data = Registrace.GetSample().Rows |> Seq.cache
 
-let myId = System.Guid("C9CA1545FBA1918CC36009B28EFB9D3B");
-let me = data |> Seq.where (fun row -> row.Id = myId)
+let dataOfInterest = data |> Seq.filter (fun row -> row.``Typ členství`` = "Řádné")
 
-let people = 
-   data
-   |> Seq.map (fun row-> row.Id)
-   |> Seq.distinct
+let dataMaxYear = 
+   dataOfInterest 
+   |> Seq.map (fun row -> row.Registrace) 
+   |> Seq.max
 
-data 
-|> Seq.groupBy (fun row -> row.Id)
-|> Seq.map (fun (id, data) -> (id, data |> Seq.maxBy (fun row -> row.Rok) |> (fun row -> row.Věk) ))
+let dataById = dataOfInterest |> Seq.groupBy (fun row -> row.Id)
+
+let allMembers = 
+   dataById
+   |> Seq.map (fun (id, rows) -> 
+      let (yearIn, ageIn) = rows |> Seq.minBy (fun row -> row.Registrace) |> (fun row -> (row.Registrace, row.Věk))
+      let (yearOut, ageOut) = rows |> Seq.maxBy (fun row -> row.Registrace) |> (fun row -> (row.Registrace, row.Věk))
+      if yearOut = dataMaxYear then
+         {id=id ; yearIn=yearIn ; ageIn=ageIn ; yearOut=None ; ageOut=None ; regs = rows }
+      else
+         {id=id ; yearIn=yearIn ; ageIn=ageIn ; yearOut=Some yearOut ; ageOut=Some ageOut ; regs = rows }
+   )
+
+let membersAlive = 
+   allMembers
+   |> Seq.where (fun stat -> stat.yearOut.IsNone)
+
+let membersAlreadyLeft = 
+   allMembers
+   |> Seq.where (fun stat -> stat.yearOut.IsSome)
+
+
+allMembers |> Seq.length
+membersAlive |> Seq.length
+membersAlreadyLeft |> Seq.length
+
+membersAlreadyLeft 
+|> Seq.where (fun mem -> (mem.yearOut = Some 2019) && (mem.ageOut < Some 18))
+|> Seq.sortBy (fun mem -> mem.ageOut)
+|> Seq.iter (fun mem -> printfn "%A" mem)
+
+let myId = @"C9CA1545FBA1918CC36009B28EFB9D3B";
+let me = dataById |> Seq.where (fun (id, data) -> id = myId)
+
