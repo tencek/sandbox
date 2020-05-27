0e361a02d4b6f774b2ce065701e518e9a50644dc
Pavel Kučera <pk250193@ncr.com>
2020-05-27 22:51:20 +0200

    Task tests

---
+++ csharp/Tasks/Tasks/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tasks
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+
+         TaskWrapper taskWrapper = new TaskWrapper();
+
+         while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+
+         taskWrapper.Start();
+
+         while (Console.ReadKey().Key != ConsoleKey.Escape) { }
+
+         taskWrapper.Stop();
+      }
+   }
+}
---
+++ csharp/Tasks/Tasks/TaskWrapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+   class TaskWrapper
+   {
+      public void Start()
+      {
+         CancellationToken cancellationToken = _cancellationTokenSource.Token;
+         _task = Task.Run(async () =>
+            {
+               while (true)
+               {
+                  Console.WriteLine("Running...");
+                  await Task.Delay(TimeSpan.FromSeconds(3.0), cancellationToken);
+                  cancellationToken.ThrowIfCancellationRequested();
+               }
+            }, _cancellationTokenSource.Token);
+      }
+
+      public void Stop()
+      {
+         if (_task != null)
+         {
+            Console.WriteLine("Stopping...");
+            _cancellationTokenSource.Cancel();
+            Console.WriteLine("Waiting...");
+            try
+            {
+               _task.Wait();
+            }
+            catch (AggregateException exception)
+            {
+               foreach (var e in exception.InnerExceptions)
+                  Console.WriteLine("{0}: {1}", e.GetType().Name, e.Message);
+            }
+         }
+      }
+
+      private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+      private Task _task;
+   }
+}
