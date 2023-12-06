f76b02c6b72a0a55bb86712bc825d55848742f85
Pavel Kučera <xedh.kucera@comap-control.com>
2023-12-06 13:40:08 +0100

    temperature_library - calculating avg

--- rust/temperature_library/src/main.rs
+++ rust/temperature_library/src/main.rs
@@ -1,30 +1,37 @@
-use temperature_library::temp::{
-    degc::DegC,
-    history::{date_range::DateRange, in_pocasi::InPocasi, loader::Loader},
-    location::Location,
+use temperature_library::temp::history::{
+    date_range::DateRange, in_pocasi::InPocasi, loader::Loader,
 };
 
 fn main() {
-    // Create a temperature value in degrees Celsius
-    let temperature = DegC::new(23.5);
-    println!("Temperature: {}°C", temperature.value);
-
     // Create a date range for history
     let date_range = DateRange {
-        start: chrono::Utc::now(),
-        end: chrono::Utc::now() + chrono::Duration::days(7),
+        start: chrono::Utc::now() - chrono::Duration::days(1),
+        end: chrono::Utc::now(),
     };
     println!("Date Range: {:?} to {:?}", date_range.start, date_range.end);
 
-    // Create a location with a name and timezone
-    let location = Location {
-        name: String::from("Sample Location"),
-        timezone: chrono_tz::Europe::Berlin,
-    };
-    println!("Location: {} ({})", location.name, location.timezone);
-
     // Use the InPocasi loader to load temperature data
-    let in_pocasi_loader = InPocasi;
-    let data = in_pocasi_loader.get_temperatures(&location, &date_range);
-    println!("Loaded temperature data: {:?}", data);
+    match InPocasi::new() {
+        Ok(loader) => loader
+            .get_available_locations()
+            .iter()
+            .for_each(|location| {
+                let temperatures = loader.get_temperatures(&location, &date_range);
+                let sum = temperatures.iter().map(|t| t.to_f64()).sum::<f64>();
+                let count = temperatures.len() as f64;
+                let avg = sum / count;
+                println!(
+                    "Location: {} avg: {:.3} ({} values)",
+                    location.name, avg, count
+                );
+            }),
+        Err(err) => {
+            println!(
+                "Could not create loader of type {}: {}",
+                std::any::type_name::<InPocasi>(),
+                err
+            );
+            return;
+        }
+    };
 }
--- rust/temperature_library/src/temp/history/in_pocasi.rs
+++ rust/temperature_library/src/temp/history/in_pocasi.rs
@@ -1,11 +1,34 @@
 use chrono::NaiveDate;
+use scraper::{Html, Selector};
+use tokio::runtime::Runtime;
 
 use crate::temp::history::date_range::dates_in_range2;
 
-use super::{date_range::DateRange, loader::Loader};
 use super::super::{degc::DegC, location::Location};
+use super::{date_range::DateRange, loader::Loader};
 
-pub struct InPocasi;
+pub struct InPocasi {
+    runtime: Runtime,
+}
+
+pub enum InPocasiError {
+    CouldNotCreateRuntime(std::io::Error),
+}
+
+impl std::fmt::Display for InPocasiError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            InPocasiError::CouldNotCreateRuntime(err) => {
+                write!(
+                    f,
+                    "Could not create runtime of type {}: {}",
+                    std::any::type_name::<Runtime>(),
+                    err
+                )
+            }
+        }
+    }
+}
 
 impl Loader for InPocasi {
     fn get_available_locations(&self) -> Vec<Location> {
@@ -17,17 +40,69 @@ impl Loader for InPocasi {
             Location {
                 name: String::from("zlin_centrum"),
                 timezone: chrono_tz::Europe::Prague,
-            }
+            },
         ]
     }
 
     fn get_temperatures(&self, location: &Location, date_range: &DateRange) -> Vec<DegC> {
-        let dates = dates_in_range2(date_range);
-        let urls:Vec<String> = dates.iter().map(|date| make_url(location, date)).collect();
-        vec![] // Placeholder return value
+        let future_data = dates_in_range2(date_range)
+            .iter()
+            .map(|date| make_url(location, date))
+            .map(|url| load_html_async(url))
+            .collect::<Vec<_>>();
+        let future_data = futures::future::join_all(future_data);
+        let data = self.runtime.block_on(future_data);
+        // let errors = data.iter().filter(|result| result.is_err());
+        let only_data = data
+            .into_iter()
+            .filter(|result| result.is_ok())
+            .map(|result| result.unwrap())
+            .collect::<Vec<_>>();
+        only_data
+            .iter()
+            .flat_map(read_temp_data)
+            .collect::<Vec<_>>()
     }
 }
 
+impl InPocasi {
+    pub fn new() -> Result<Self, InPocasiError> {
+        let runtime =
+            tokio::runtime::Runtime::new().map_err(InPocasiError::CouldNotCreateRuntime)?;
+        Ok(InPocasi { runtime })
+    }
+}
+
+fn read_temp_data(html: &Html) -> Vec<DegC> {
+    let table = Selector::parse("table").unwrap(); // todo - error handling
+    let tbody = Selector::parse("tbody").unwrap(); // todo - error handling
+    let tr = Selector::parse("tr").unwrap(); // todo - error handling
+    let td = Selector::parse("td").unwrap(); // todo - error handling
+
+    html.select(&table)
+        .next()
+        .unwrap()
+        .select(&tbody)
+        .next()
+        .unwrap()
+        .select(&tr)
+        .map(|row| {
+            let temp_str = row.select(&td).nth(1).unwrap().inner_html();
+            DegC::from_str(&temp_str).unwrap()
+        })
+        .collect::<Vec<_>>()
+}
+
+async fn load_html_async(url: String) -> Result<Html, reqwest::Error> {
+    let response_text = reqwest::get(url).await?.text().await?;
+    let document = Html::parse_document(&response_text);
+    Ok(document)
+}
+
 fn make_url(location: &Location, date: &NaiveDate) -> String {
-    format!("http://www.in-pocasi.cz/aktualni-pocasi/{}/?historie={}", location.name, date.format("%Y-%m-%d"))
+    format!(
+        "http://www.in-pocasi.cz/aktualni-pocasi/{}/?historie={}",
+        location.name,
+        date.format("%Y-%m-%d")
+    )
 }
