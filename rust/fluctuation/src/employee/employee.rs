c7079ab5a2f57e820a5ce8514dfb3b6ab7c00d73
Pavel Kučera <pavel.kucera1@thermofisher.com>
2024-03-06 09:24:44 +0100

    Add num_years_by method to Employee struct

--- rust/fluctuation/src/employee/employee.rs
+++ rust/fluctuation/src/employee/employee.rs
@@ -20,11 +20,19 @@ impl Employee {
         self.board_out.is_none()
     }
 
-    pub fn was_active_at(&self, date: NaiveDate) -> bool {
+    pub fn was_active_by(&self, date: NaiveDate) -> bool {
         if let Some(board_out) = self.board_out {
             date >= self.board_in && date <= board_out
         } else {
             date >= self.board_in
+        }
+    }
+
+    pub fn num_years_by(&self, date: NaiveDate) -> Option<f64> {
+        if self.was_active_by(date) {
+            Some(date.signed_duration_since(self.board_in).num_days() as f64 / 365.25)
+        } else {
+            None
         }
     }
 }
@@ -52,6 +60,7 @@ impl TryFrom<StringRecord> for Employee {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use approx::*;
     use chrono::NaiveDate;
 
     #[test]
@@ -61,15 +70,53 @@ mod tests {
             Some(NaiveDate::from_ymd_opt(2020, 12, 31).unwrap()),
         );
 
-        assert!(employee.was_active_at(NaiveDate::from_ymd_opt(2020, 1, 1).unwrap()));
-        assert!(employee.was_active_at(NaiveDate::from_ymd_opt(2020, 12, 31).unwrap()));
-        assert!(employee.was_active_at(NaiveDate::from_ymd_opt(2020, 6, 1).unwrap()));
-        assert!(!employee.was_active_at(NaiveDate::from_ymd_opt(2021, 1, 1).unwrap()));
+        assert!(employee.was_active_by(NaiveDate::from_ymd_opt(2020, 1, 1).unwrap()));
+        assert!(employee.was_active_by(NaiveDate::from_ymd_opt(2020, 12, 31).unwrap()));
+        assert!(employee.was_active_by(NaiveDate::from_ymd_opt(2020, 6, 1).unwrap()));
+        assert!(!employee.was_active_by(NaiveDate::from_ymd_opt(2021, 1, 1).unwrap()));
     }
 
     #[test]
     fn naive_date_from_string() {
         let date = NaiveDate::parse_from_str("2023-05-01", "%Y-%m-%d").ok();
         assert_eq!(date, NaiveDate::from_ymd_opt(2023, 5, 1));
+    }
+
+    #[test]
+    fn test_num_years_by() {
+        let employee = Employee::new(
+            NaiveDate::from_ymd_opt(2020, 1, 1).unwrap(),
+            Some(NaiveDate::from_ymd_opt(2020, 12, 31).unwrap()),
+        );
+
+        assert_relative_eq!(
+            employee
+                .num_years_by(NaiveDate::from_ymd_opt(2020, 1, 1).unwrap())
+                .unwrap(),
+            0.0,
+            epsilon = 0.001
+        );
+        assert_relative_eq!(
+            employee
+                .num_years_by(NaiveDate::from_ymd_opt(2020, 12, 31).unwrap())
+                .unwrap(),
+            0.999,
+            epsilon = 0.001
+        );
+        assert_relative_eq!(
+            employee
+                .num_years_by(NaiveDate::from_ymd_opt(2020, 6, 1).unwrap())
+                .unwrap(),
+            0.416,
+            epsilon = 0.001
+        );
+        assert_eq!(
+            employee.num_years_by(NaiveDate::from_ymd_opt(2021, 1, 1).unwrap()),
+            None
+        );
+        assert_eq!(
+            employee.num_years_by(NaiveDate::from_ymd_opt(2019, 1, 1).unwrap()),
+            None
+        );
     }
 }
--- rust/fluctuation/src/main.rs
+++ rust/fluctuation/src/main.rs
@@ -1,21 +1,19 @@
 mod assets;
 mod employee;
-
-use employee::Employee;
 use employee::Employees;
 
 fn main() {
+    use chrono::Local;
     use itertools::Itertools;
     use std::iter::once;
 
     match Employees::try_from(assets::EMPLOYEES) {
         Ok(employees) => {
-            let active: Vec<&Employee> = employees
-                .iter()
-                .filter(|employee| employee.is_active())
-                .collect();
-            println!("Active employees: {:?}", active.len());
-            let special_dates_sorted = employees
+            let mut wtr = csv::Writer::from_path("active_employees_by_date.csv").unwrap();
+            wtr.write_record(&["date", "active_count", "num_years_avg"])
+                .unwrap();
+
+            employees
                 .iter()
                 .flat_map(|e| {
                     if e.is_active() {
@@ -30,10 +28,39 @@ fn main() {
                             .chain(once(e.board_out.unwrap().succ_opt().unwrap()))
                     }
                 })
+                .chain(once(Local::now().date_naive()))
                 .unique()
                 .sorted()
-                .collect::<Vec<_>>();
-            println!("Distinct dates: {:?}", special_dates_sorted.len());
+                .map(|date| {
+                    let active_employees =
+                        employees.iter().filter(|e| e.was_active_by(date.clone()));
+                    let active_count = active_employees.clone().count();
+                    let num_years_avg = active_employees
+                        .map(|e| e.num_years_by(date.clone()).unwrap())
+                        .sum::<f64>()
+                        / active_count as f64;
+                    (date, active_count, num_years_avg)
+                })
+                .for_each(|(date, active_count, num_years_avg)| {
+                    wtr.write_record(&[
+                        date.to_string(),
+                        active_count.to_string(),
+                        num_years_avg.to_string(),
+                    ])
+                    .unwrap();
+                });
+
+            wtr.flush().unwrap();
+
+            let active_employees = employees.iter().filter(|e| e.is_active());
+            let active_employee_count = active_employees.clone().count();
+            let num_years_avg = active_employees
+                .clone()
+                .map(|e| e.num_years_by(Local::now().date_naive()).unwrap())
+                .sum::<f64>()
+                / active_employee_count as f64;
+            println!("Active employees: {}", active_employee_count);
+            println!("Average number of years: {:.3}", num_years_avg);
         }
         Err(e) => eprintln!("Error: {}", e),
     }
